719bc3f0267f6bf4f87bcac344cc28b449faae88
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-21 23:18:53 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname+ '/dist/serviceapp'));		// dist/'file package name'
+app.get('/*',function(req,res) {
+		res.sendFile(path.join(__dirname+'/dist/serviceapp/index.html')); 	//here also name change as per app name
+	});
+	app.listen(process.env.PORT || 8080 )
+
