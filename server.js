e2c1d9b235e655da0edbbe2f6ca6815df6611960
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-22 11:15:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+ '/dist/serviceapp'));		// dist/'file package name'
+app.use(express.static(__dirname+ '/dist/timeapp'));		// dist/'file package name'
 app.get('/*',function(req,res) {
 		res.sendFile(path.join(__dirname+'/dist/serviceapp/index.html')); 	//here also name change as per app name
 	});
